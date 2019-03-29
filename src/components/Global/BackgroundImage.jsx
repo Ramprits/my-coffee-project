43dd213a210b80eaf087c6c14116beff1dd269ac
ramprits <rampritsahani@outlook.in>
2019-03-29 07:03:52 +0530

    typed adds

--- src/components/Global/BackgroundImage.jsx
+++ src/components/Global/BackgroundImage.jsx
@@ -1,10 +1,16 @@
 import React from "react"
 import BackgroundImage from "gatsby-background-image"
+import Typed from "react-typed"
 
 const BackgroundImageSection = ({ imageData, className }) => {
   return (
     <BackgroundImage className={className} fluid={imageData}>
-      <h2 className="text-white font-weight-bold">Welcome to Coffee shop</h2>
+      <Typed
+        strings={["Coffe", "Hot Tea", "Green Tea"]}
+        typeSpeed={80}
+        loop
+        className="text-primary font-weight-bold text-center"
+      />
     </BackgroundImage>
   )
 }
